47871aa6cf889832acfbb3fc2838c87ea9626219
Henadzi Arashkevich <oreshkevich85@mail.ru>
2021-09-16 12:21:28 +0300

    start

---
+++ wordpress_7gcmk/public_html/wp-content/themes/wpgame/functions.php
@@ -0,0 +1,16 @@
+<?php
+// правильный способ подключить стили и скрипты
+add_action('wp_enqueue_scripts', 'wpgame_scripts');
+// add_action('wp_print_styles', 'wpgame_scripts'); // можно использовать этот хук он более поздний
+function wpgame_scripts()
+{
+  wp_enqueue_style('style', get_stylesheet_uri());
+  wp_enqueue_style('style-min', get_template_directory_uri() . '/css/style.css');
+  wp_enqueue_script('script', get_template_directory_uri() . '/js/script.js', array(), '1.0.0', true);
+}
+add_theme_support('post-thumbnails', array('post'));
+add_action('after_setup_theme', 'theme_register_nav_menu');
+function theme_register_nav_menu()
+{
+  register_nav_menu('primary', 'Primary Menu');
+}
